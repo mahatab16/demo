642766534bec870abf966be759151eb467de12c7
Mahatab Mallick <Mahatab Mallick>
2016-01-17 19:50:21 +0530

    swap.py update print value before swap

--- swap.py
+++ swap.py
@@ -3,13 +3,13 @@
 
 x = 333
 y = 111
+print('The value os x before swaping: {}'.format(x))
+print('The value os y before swaping: {}'.format(y))
+
 
 temp = x
 x = y
 y = temp
-
-print('The value os x before swaping: {}'.format(x))
-print('The value os y before swaping: {}'.format(y))
 
 print('The value of x after swapping: {}'.format(x))
 print('The value of y after swapping: {}'.format(y))
