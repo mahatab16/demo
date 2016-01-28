316ed2f4d6242f0422b5de416bb62ad6e4563786
mahatab <mohammad.mallick1@wipro.com>
2016-01-28 22:23:53 +1100

    update remove format swap.py

--- swap.py
+++ swap.py
@@ -1,12 +1,12 @@
 x = 333
 y = 111
-print('The value of x before swaping: {}'.format(x))
-print('The value of y before swaping: {}'.format(y))
+print('The value of x before swaping: {}' x)
+print('The value of y before swaping: {}' y)
 
 
 temp = x
 x = y
 y = temp
 
-print('The value of x after swapping: {}'.format(x))
-print('The value of y after swapping: {}'.format(y))
+print('The value of x after swapping: {}' x)
+print('The value of y after swapping: {}' y)
